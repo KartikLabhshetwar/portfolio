2f8fb1b4deb39aec7710f63e3c4120e74a2d6d50
Kartik Labhshetwar <kartik.labhshetwar@gmail.com>
2024-10-11 21:27:36 +0530

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -19,7 +19,7 @@ const Header: React.FC = () => {
     };
   }, []);
 
-  const resumeLink = "https://drive.google.com/file/d/14C2Yoa1yDk8tCuoKHt9-jtLJlAXWu7Dg/view?usp=sharing";
+  const resumeLink = "https://drive.google.com/file/d/1y2sJE90XdMLI8Z2vQyu4aXFpPAav1XWZ/view?usp=sharing";
 
   return (
     <header className={`fixed top-0 left-0 right-0 z-50 transition-all duration-300 ${isScrolled ? 'bg-background shadow-md' : 'bg-transparent'}`}>
